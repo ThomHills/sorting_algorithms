5fe9dac90d4109fb32f5e753d63502d8f1af14ae
Thom <thomphiri9@gmail.com>
2023-02-18 23:16:13 -0800

    Deck

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,41 +1,72 @@
 #include "sort.h"
+
 /**
-  * heap_sort - heap sort algorithm
-  * @array: array to sort
-  * @size: size of array
-  */
+ * heap_sort - Sorting array using heap sort algorithm
+ * @array: Array to be sorted
+ * @size: Size of the array
+ * Return: 0
+ */
 void heap_sort(int *array, size_t size)
 {
-	int i, tmp;
+	int i;
 
-	for (i = size / 2 - 1; i >= 0; i--)
-		heapify(array, i, size, size);
-	for (i = size -1; i >=0; i--)
+	if (!array || size < 2)
+		return;
+
+	for (i = size / 2; i >= 0; i--)
+		heapify(array, size, i, size);
+	for (i = size - 1; i >= 0; i--)
 	{
-		tmp = array[0];
-		array[0] = array[i];
-		array[i] = tmp;
-		print_array(array, size);
+		swap(&array[i], &array[0]);
+		if (i != 0)
+			print_array(array, size);
 		heapify(array, i, 0, size);
 	}
 }
-void heapify(int *array, int idx, int idx2, size_t size)
+
+/**
+ * heapify - Recursive function to sort binary tree
+ * @array: array to be sorted as binary tree
+ * @end: Last node in binary tree
+ * @start: First node of binary tree
+ * @size: Size of the array to sort
+ * Return: 0
+ */
+void heapify(int *array, int end, int start, size_t size)
 {
-	int max = idx2;
-	int left = 2 * idx2 + 1;
-	int right = 2 * idx2 + 2;
+	int max = start;
+	int left = 2 * start + 1;
+	int right = 2 * start + 2;
+
+	if (!array || size < 2)
+		return;
+
+	if (left < end && array[left] > array[max])
+		max = left;
+
+	if (right < end && array[right] > array[max])
+		max = right;
+
+	if (start != max)
+	{
+		swap(&array[start], &array[max]);
+		print_array(array, size);
+		heapify(array, end, max, size);
+	}
+}
+
+/**
+ * swap - Function that swaps two values
+ *
+ * @a: Fisrt value
+ * @b: Second value
+ * Return: 0
+ */
+void swap(int *a, int *b)
+{
 	int tmp;
 
-	if (left < idx && array[left] > array[max])
-		max = left;
-	if (right < idx && array[right] > array[max])
-		max = right;
-	if (max != idx2)
-	{
-		tmp = array[idx2];
-		array[idx2] = array[max];
-		array[max] = tmp;
-		print_array(array, size);
-		heapify(array, idx, max, size);
-	}
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
 }
